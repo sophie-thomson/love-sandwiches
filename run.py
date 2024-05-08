33a9eb94aed6b4ae8054860d982f33b756255812
Sophie Thomson <sophielbenoit@gmail.com>
2024-05-08 11:39:41 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -29,21 +29,24 @@ def get_sales_data():
     """
     Function to get sales figures input from user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")#\n added to create space between instructions and place to enter data
+    while True: #loop to run same code every time an error is raised 
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")#\n added to create space between instructions and place to enter data
 
-    data_str = input("Enter your data here: ")
-    # print(f"The data provided is {data_str}") #print statement only used to check that input feature working
+        data_str = input("Enter your data here: ")
+        # print(f"The data provided is {data_str}") #print statement only used to check that input feature working
 
-    # split() method returns the broken up values as a list instead of one long string
-    # each object is defined by the "," parameter provided in the split() function
-    sales_data = data_str.split(",")
-    # print(sales_data) #print statement only used to check that string split is working
+        # split() method returns the broken up values as a list instead of one long string
+        # each object is defined by the "," parameter provided in the split() function
+        sales_data = data_str.split(",")
+        # print(sales_data) #print statement only used to check that string split is working
 
-    # call validate_data() within the get_sales_data function
-    validate_data(sales_data) #sales_data is the data that we want to check
-
+        # call validate_data() within the get_sales_data function
+        # if statement to close while loop --- while True, it keeps 
+        if validate_data(sales_data): #sales_data is the data that we want to check
+            print("Data is valid!")
+            break 
 
 def validate_data(values):
     """
@@ -59,6 +62,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False #if error is raised, returns False
+    
+    return True #if function runs without any errors, then returns True 
 
 
     # print(values) #print statement used to check if values printing from 
