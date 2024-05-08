677e8759a968fba19157fbc17d2f873ebe8c1f75
Sophie Thomson <sophielbenoit@gmail.com>
2024-05-08 11:16:07 +0000

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -16,10 +16,11 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-# #Calls sales tab from the worksheet
+# ------ TEST IF THE API IS WORKING AND DATA PASSING FROM SHEET ------ #
+# # Calls sales tab from the worksheet
 # sales = SHEET.worksheet("sales")
 
-# #Built-in function get_all_values collects all of the data from that tab in the worksheet
+# # Built-in function get_all_values collects all of the data from that tab in the worksheet
 # data = sales.get_all_values()
 
 # print(data)
@@ -33,7 +34,34 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")#\n added to create space between instructions and place to enter data
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    # print(f"The data provided is {data_str}") #print statement only used to check that input feature working
+
+    # split() method returns the broken up values as a list instead of one long string
+    # each object is defined by the "," parameter provided in the split() function
+    sales_data = data_str.split(",")
+    # print(sales_data) #print statement only used to check that string split is working
+
+    # call validate_data() within the get_sales_data function
+    validate_data(sales_data) #sales_data is the data that we want to check
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all tring values into integers.
+    Raises ValueError if strings cannot be converted into int, 
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+    # print(values) #print statement used to check if values printing from 
+    # within the validate_data function instead of 
 
 #Call the function to run
 get_sales_data()
